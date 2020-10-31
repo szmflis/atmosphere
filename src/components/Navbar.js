ec90ab914a314b97c1dd841ea3b846fafcb3d283
szmflis98 <szmflis@gmail.com>
2020-10-31 13:54:58 +0100

    navbar blue line update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { FlexContainer } from '../elements/FlexContainer'
 import { theme } from '../styles/theme'
 
 const Wrapper = styled.nav`
   display: flex;
+  flex-direction: column;
   align-items: center;
   width: 100%;
-  padding: 1rem;
+
+  border-top: 4px solid ${theme.colors.primary};
+`
+
+const WidthWrapper = styled(FlexContainer)`
+  width: 60vw;
+  max-width: 1080px;
+
+  @media ( max-width: 1080px ) {
+    width: 600px;
+  }
+
+  @media ( max-width: 650px ) {
+    width: 90%;
+  }
 `
 
 const NavElementWrapper = styled.div`
@@ -24,10 +40,10 @@ const NavElement = styled(Link)`
   /* Typography */
   color: ${theme.colors.black};
   font-size: ${theme.fontSize.big};
-  font-weight: ${theme.fontWeight.semibold};
+  font-weight: ${theme.fontWeight.regular};
 
   /* Spacing */
-  margin: 0rem 3rem;
+  margin: .5rem;
   padding: 1rem;
 
   /* Underline animation */
@@ -44,7 +60,6 @@ const NavElement = styled(Link)`
   transition: opacity .3s, background-size .3s;
 
   @media ( max-width: 640px ) {
-    margin: 0;
     font-size: 4vw;
   }
 `
@@ -52,14 +67,16 @@ const NavElement = styled(Link)`
 const Navbar = () => {
   return (
     <Wrapper>
-      <NavElementWrapper>
-        <NavElement to="/">atmosphere</NavElement>
-      </NavElementWrapper>
-      <NavElementWrapper>
-      </NavElementWrapper>
-      <NavElementWrapper justify="flex-end">
-        <NavElement to="/city">City</NavElement>
-      </NavElementWrapper>
+      <WidthWrapper row>
+        <NavElementWrapper>
+          <NavElement to="/">atmosphere</NavElement>
+        </NavElementWrapper>
+        <NavElementWrapper>
+        </NavElementWrapper>
+        <NavElementWrapper justify="flex-end">
+          <NavElement to="/city">City</NavElement>
+        </NavElementWrapper>
+      </WidthWrapper>
     </Wrapper>
   )
 }
